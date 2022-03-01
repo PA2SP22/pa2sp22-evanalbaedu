6baac827245993b69bd30c5998035edaa3759084
Ubuntu <ubuntu@ip-172-31-95-104.ec2.internal>
2022-03-01 18:43:15 +0000

    Adding Lab #9

--- labs/9/lab_9.cpp
+++ labs/9/lab_9.cpp
@@ -1,11 +1,12 @@
 /*
  * Name        : lab_9.cpp
- * Author      : FILL IN
+ * Author      : Evan Alba
  * Description : Working with Classes
  */
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <iomanip>
 using std::cout;
 using std::endl;
 using std::string;
@@ -106,8 +107,46 @@ int main() {
 }
 
 // CODE HERE -- CLASS DEFINITION
+void Spaceship::set_name(string name) {
+  name_ = name;
+}
 
+void Spaceship::set_top_speed(double top_speed) {
+  top_speed_ = top_speed;
+}
 
+void Spaceship::set_fuel_source(string fuel_source) {
+  fuel_source_ = fuel_source;
+}
+
+void Spaceship::set_crew_capacity(int crew_capacity) {
+  crew_capacity_ = crew_capacity;
+}
+
+string Spaceship::name() const {
+  return name_;
+}
+
+double Spaceship::top_speed() const {
+  return top_speed_;
+}
+
+string Spaceship::fuel_source() const {
+  return fuel_source_;
+}
+
+int Spaceship::crew_capacity() const {
+  return crew_capacity_;
+}
+
+string Spaceship::ToString() const {
+  stringstream s;
+  s << name() << "\nTop Speed:     Warp "
+  << std::fixed << std::setprecision(2) << top_speed()
+  << "\n" << "Fuel Source:   " << fuel_source()
+  << "\nCrew Capacity: " << crew_capacity();
+  return s.str();
+}
 // For testing (DO NOT ALTER)
 void UnitTest() {
   cout << string(40, '-') << endl;
