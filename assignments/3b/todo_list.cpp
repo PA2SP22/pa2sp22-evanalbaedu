647eff796954c5b8d265718145bd814236ce04c2
Ubuntu <ubuntu@ip-172-31-14-6.ec2.internal>
2022-03-29 01:54:35 +0000

    Safety #2

--- assignments/3b/todo_list.cpp
+++ assignments/3b/todo_list.cpp
@@ -4,26 +4,75 @@
  * Description : CPP File for class TodoList.
  */
 #include "todo_list.h"
-#include "todo_item.cpp"
 
-void TodoList::AddItem(TodoItem* add = new TodoItem()) {
+void TodoList::AddItem(TodoItem* add) {
   if (size_ == cap_) {
-    cap_ += 10;
+    IncreaseCap();
   }
   for (int i = 0; i < cap_; i++) {
     if (list_[i] == nullptr) {
       list_[i] = add;
+      size_ += 1;
       break;
     }
   }
 }
-void TodoList::DeleteItem(int area) {}
-int* TodoList::GetItem(int spot) {}
+
+void TodoList::DeleteItem(int area) {
+  if (list_[area-1] != nullptr) {
+    list_[area-1] = nullptr;
+    TightenArray(); 
+  }
+}
+
+TodoItem* TodoList::GetItem(int spot) {
+  if (list_[spot-1] == nullptr) {
+    return nullptr;
+  }
+  return list_[spot-1];
+}
+
 unsigned int TodoList::GetSize() const {
   return size_;
 }
+
 unsigned int TodoList::GetCapacity() const {
   return cap_;
 }
+
 void TodoList::Sort() {}
+
 std::string TodoList::ToFile() {}
+
+std::ostream& operator << (std::ostream &out, const TodoList &obj) {}
+
+/* PRIVATE */
+void TodoList::IncreaseCap() {
+  cap_ += 10;
+}
+
+void TodoList::TightenArray() {
+  if (size_  >= 1) {
+    int need = size_;
+    for (int i = 0; i < cap_; i++) {
+      
+      if (need > 0) {
+    
+        if (list_[i] == nullptr) {
+          // Look for number
+          for (int x = 0; i < cap_; i++) {
+            if (list_[x] != nullptr) {
+              list_[i] = list_[x];
+              break;
+            }
+          }
+          need -= 1;
+        }
+        
+      } else {
+        break;
+      }
+      
+    }
+  }
+}
--- assignments/3b/todo_list.h
+++ assignments/3b/todo_list.h
@@ -5,30 +5,36 @@
  */
 #ifndef TODO_LIST
 #define TODO_LIST
+#include "todo_item.h"
 #include <iostream>
 #include <string>
 
 class TodoList {
  public:
     TodoList() {
-      list_ = new std::string[cap_];
+      list_ = new TodoItem*[cap_];
       for (unsigned int i = 0; i < cap_; i++) {
         list_[i] = nullptr;
       }
     }
     
     ~TodoList() {}
-    void AddItem(TodoItem* add = new TodoItem());
+    
+    void AddItem(TodoItem* add);
     void DeleteItem(int area);
-    int* GetItem(int spot);
+    TodoItem* GetItem(int spot);
     unsigned int GetSize() const;
     unsigned int GetCapacity() const;
+    
     void Sort();
     std::string ToFile();
+    friend std::ostream& operator << (std::ostream &out, const TodoList &obj);
   
  private:
     unsigned int size_ = 0; /* Current size of your list. */
     unsigned int cap_ = 25; /* Max capacity of your list. */
-    std::string* list_; /* Pointer to TodoItem*. */
+    TodoItem** list_; /* Pointer to TodoItem*. */
+    void IncreaseCap();
+    void TightenArray();
 };
 #endif
