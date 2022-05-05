143bd92f7769a675d5c8d49458b1185b58ee1e2c
evanalba <evanalba1403@gmail,com>
2022-05-05 02:38:33 -0700

    5/5 Night Assignment #4 progress.

--- assignments/4/dl_list.cpp
+++ assignments/4/dl_list.cpp
@@ -19,24 +19,37 @@ void DLList::PushFront(int num) {
   size_ += 1;
   new_node->SetContents(num);
   if (head_ == nullptr) {
-    tail_ = head_;
+    new_node->SetPrevious(nullptr);
+    new_node->SetNext(nullptr);
+    head_ = new_node;
+    tail_ = new_node;
+  } else {
+    DLNode* old_head = head_;
+    old_head->SetPrevious(new_node);
+    new_node->SetPrevious(nullptr); 
+    new_node->SetNext(old_head);
+    head_ = new_node;
   }
-  new_node->SetNext(head_);
-  if (head_ != nullptr) {
-    head_->SetPrevious(new_node);
-  }
-  head_ = new_node;
-} 
+}
 
 void DLList::PushBack(int num) {
   if (head_ == nullptr) {
-    PushFront(num);
+    DLNode* new_node = new DLNode();
+    size_ += 1;
+    new_node->SetContents(num);
+    new_node->SetPrevious(nullptr);
+    new_node->SetNext(nullptr);
+    head_ = new_node;
+    tail_ = new_node;
   } else {
     DLNode* new_node = new DLNode();
     size_ += 1;
     new_node->SetContents(num);
-    new_node->SetNext(head_);
-    tail_ = new_node;
+    DLNode* old_tail = tail_;
+    old_tail->SetNext(new_node);
+    new_node->SetPrevious(old_tail);
+    new_node->SetNext(nullptr);
+    tail_ = new_node; 
   }
  }
 
@@ -65,8 +78,8 @@ void DLList::PopFront() {
   head_ = head_->GetNext();
   delete first;
   size_ -= 1;
-  if (head_ == nullptr) {
-    tail_ = nullptr;
+  if (head_ != nullptr) {
+    head_->SetPrevious(nullptr);
   }
 }
 
@@ -75,13 +88,38 @@ void DLList::PopBack() {
     std::cerr << "List Empty";
     return;
   }
+  DLNode* last = tail_;
+
+//  tail_ = tail_->GetPrevious();
+//  delete last;
+//  size_ -= 1;
+//  tail_->SetNext(nullptr);
 }
 
 void DLList::RemoveFirst(int num) {
+  DLNode* it = head_;
+  DLNode* trailer = head_;
+  while (it != nullptr && num != it->GetContents()) {
+    trailer = it;
+    it = it->GetNext();
+  }
+  if (it == nullptr) {
+    std::cerr << "Not Found";
+  } else if (it == head_) {
+    PopFront();
+  } else if (it == tail_) {
+    PopBack();
+  } else {
+    trailer->SetNext(it->GetNext());
+    delete it;
+    size_ -= 1;
+  }
   std::cerr << "Not Found";
 }
 
 void DLList::RemoveAll(int num) {
+// Look through list
+// loop # of times using RemoveFirst()
   std::cerr << "Not Found";
 }
 
@@ -96,10 +134,22 @@ bool DLList::Exists(int num) {
   return false;
 }
 
-void DLList::Clear() {}
+void DLList::Clear() {
+  if (head_ == nullptr) {
+    return;
+  }
+  DLNode* it = head_;
+  while (it != nullptr) {
+    head_ = head_->GetNext();
+    delete it;
+    it = head_;
+  }
+  size_ = 0;
+  tail_ = nullptr;
+}
 
 std::string DLList::ToStringForwards() {
-  if (head_ == nullptr) {
+  if (size_ == 0) {
     std::cerr << "List Empty"; 
     return "";
   }
@@ -118,9 +168,21 @@ std::string DLList::ToStringForwards() {
 }
 
 std::string DLList::ToStringBackwards() {
- if (size_ == 0) {
+  if (size_ == 0) {
     std::cerr << "List Empty"; 
     return "";
   }
+  std::stringstream s;
+  DLNode* back  = tail_;
+  while (back != nullptr) {
+    if (back->GetPrevious() == nullptr) {
+      s << back->GetContents();
+    } else {
+      s << back->GetContents() << ", ";
+    }
+    back = back->GetPrevious();
+  }
+  std::cout << s.str() << std::endl;
+  return s.str();
 }
 
