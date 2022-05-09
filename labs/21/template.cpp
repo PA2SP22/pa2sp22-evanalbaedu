a71a80b07b78772d085d4ff190b5b2be4e0bb68d
evanalba <evanalba1403@gmail,com>
2022-05-09 15:54:20 -0700

    Lab #21 Uploaded.

--- labs/21/template.cpp
+++ labs/21/template.cpp
@@ -1,11 +1,13 @@
 /*
  * Name        : template.cpp
- * Author      : FILL IN
+ * Author      : Evan Alba
  * Description : Various functions using templates. Please note you can just
  *               write the definitions in here. No need for prototypes.
  */
+#include <iostream>
 #include <string>
 #include <sstream>
+#include <iomanip>
 using std::string;
 using std::stringstream;
 
@@ -24,6 +26,18 @@ using std::stringstream;
  * @return string - A string containing the contents of values separated by the
  *                  specified separator character
  */
+template <typename T>
+string PrepareForDisplay(T values[], unsigned int size, char separator=',') {
+  stringstream s;
+  for (unsigned int i = 0; i < size; i++) {
+    if (i != (size - 1)) {
+      s << std::setprecision(2) << std::fixed << values[i] << separator;
+    } else {
+      s << std::setprecision(2) << std::fixed << values[i];
+    }
+  }
+  return s.str();
+}
 
 
 /*
@@ -34,6 +48,15 @@ using std::stringstream;
  * @param T value_to_find - The value to search for within the array
  * @return bool - true if value is found in the array, otherwise false
  */
+template <typename T>
+bool HasValue(T values[], unsigned int size, T value_to_find) {
+  for (unsigned int i = 0; i < size; i++) {
+    if (values[i] == value_to_find) {
+      return true;
+    }
+  }
+  return false;
+}
 
 
 /*
@@ -50,6 +73,15 @@ using std::stringstream;
  *               the type and sets error to true. To get a zero representation
  *               you can return T()
  */
+template<typename T>
+T ValueAt(T values[], unsigned int size, unsigned int index, bool &error) {
+  if (index > size - 1) {
+    error = true;
+    return T();
+  }
+  error = false;
+  return values[index];
+}
 
 
 /*
@@ -60,6 +92,14 @@ using std::stringstream;
  * @param unsigned int size - The size of the array
  * @return T - The sum of the values in the array
  */
+template<typename T>
+T Sum(T values[], unsigned int size) {
+  T sum = T();
+  for (unsigned int i = 0; i < size; i++) {
+    sum += values[i];
+  }
+  return sum;
+}
 
 
 /*
@@ -72,4 +112,15 @@ using std::stringstream;
  * @param unsigned int index2 - The position of the second value to be swapped
  * @return bool - true if the swap was successful, otherwise false
  */
+template<typename T>
+bool SwapValues(T values[], unsigned int size, unsigned int index1,
+unsigned int index2) {
+  if ( (index1 > size) || (index2 > size) ) {
+    return false;
+  }
+  T temp = values[index1];
+  values[index1] = values[index2];
+  values[index2] = temp;
+  return true;
+}
 
